feb5577e32a83b615cb29ddca3d59aae6655dcca
jcarlo123 <jcg.ricablanca@gmail.com>
2017-11-12 17:35:11 +0800

    Server now able to handle multiple clients

--- src/Client.java
+++ src/Client.java
@@ -12,28 +12,32 @@ public class Client {
 			// Instantiate a new socket for the client
 			// using the arguments serverID and port
 			Socket userClient = new Socket(serverID, roomPort);
+			// Receive data from the server
+			InputStream fromServer = userClient.getInputStream();
+			DataInputStream in = new DataInputStream(fromServer);
+			System.out.println(userClient.getRemoteSocketAddress() + ": " + in.readUTF());
+
 			while(true) {
 				// Read the input of the user and send it to the 
 				// socket of the server
 				OutputStream toServer = userClient.getOutputStream();
 				DataOutputStream out = new DataOutputStream(toServer);
-				BufferedReader buffer = new BufferedReader(
-											new InputStreamReader(System.in));
+				
+				// Read input from the user
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 				String message = buffer.readLine();
+				
 				// Exit chat when "exit" is detected
 				if(message.equals("exit")) {
+					//Send exit message
+					out.writeUTF("exit");
 					break;
 				}
 				out.writeUTF(userClient.getLocalSocketAddress() + ": " + message);
-				// Receive data from the server
-				InputStream fromServer = userClient.getInputStream();
-				DataInputStream in = new DataInputStream(fromServer);
-				System.out.println(userClient.getRemoteSocketAddress() + ": " + 
-					in.readUTF());
 			}
 
 			userClient.close();
-			// Accept input from the user
+			
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
--- src/Server.java
+++ src/Server.java
@@ -1,53 +1,31 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class Server extends Thread{
-  private ServerSocket serverSocket;
+public class Server{
+   public static ServerSocket serverSocket;
+    
+    public static void acceptClients(){
+        ArrayList<ClientThread> clients =  new ArrayList<ClientThread>();
 
-  public Server(int port) throws IOException{
-    serverSocket = new ServerSocket(port);
-    //serverSocket.setSoTimeout(10000);
-  }
-
-  public void run(){
-    while(true){
-      try
-      {
-        System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
-
-        /* Start accepting data from the ServerSocket */
-        Socket server = serverSocket.accept();
-        System.out.println("Just connected to " + server.getRemoteSocketAddress());
-
-        /* Read data from the ClientSocket */
-        DataInputStream in = new DataInputStream(server.getInputStream());
-
-        while(server.getInputStream()!=null){
-          System.out.println(in.readUTF());
-          DataOutputStream out = new DataOutputStream(server.getOutputStream());
-          out.writeUTF();
+        while(true){
+            try {
+                Socket socket = serverSocket.accept();
+                ClientThread client = new ClientThread(socket);
+                Thread t = new Thread(client);
+                t.start();
+                clients.add(client);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        
-
-        /* Send data to the ClientSocket */
-        
-        server.close();
-
-      }catch(SocketTimeoutException s){
-        System.out.println("Socket timed out!");
-        break;
-      }catch(IOException e){
-        e.printStackTrace();
-        break;
-      }
     }
-  }
 
-  public static void main(String [] args){
+    public static void main(String [] args){
     int port = Integer.parseInt(args[0]);
     try {
-      Thread t = new Server(port);
-      t.start();
+      serverSocket = new ServerSocket(port);
+      acceptClients();
     }catch(IOException e){
       e.printStackTrace();
     }
